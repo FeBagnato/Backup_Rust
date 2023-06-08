dcf2b49874e73f37fe48628e7f85fee7a1109adf
FeBagnato <felipebag@protonmail.com>
2023-06-07 23:39:03 -0300

    :sparkles: Implementing code to save and compress the files

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use rpassword;
 
+mod backup;
+
 fn main() {
     println!("\x1b[33mCaso tenha algum arquivo ou pasta que você não queira adicionar ao backup, 
 coloque o caminho em \"config/ignore_list.conf\"\x1b[0m\n");
@@ -11,7 +13,12 @@ coloque o caminho em \"config/ignore_list.conf\"\x1b[0m\n");
         if password == rpassword::prompt_password("Digite a senha novamente: ").unwrap() {
             //TODO: Run verify code here!
             //TODO: Run ignore_list code here!
-            //TODO: Run backup code here!
+            backup::init("Desktop", &password);
+            backup::init("Documentos", &password);
+            backup::init("Downloads", &password);
+            backup::init("Imagens", &password);
+            backup::init("Música", &password);
+            backup::init("Vídeos", &password);
             pass_error = false;
         }
         else{
