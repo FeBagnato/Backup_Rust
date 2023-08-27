3e829493738790328f860c10901368812cb3c919
FeBagnato <felipebag@protonmail.com>
2023-08-27 11:55:30 -0300

    :sparkles: Implementing threads to speed up the backup

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use rpassword;
+use std::{thread::{self, JoinHandle}, sync::Arc};
 
 mod backup;
 mod ignore;
@@ -7,6 +8,8 @@ fn main() {
     println!("\x1b[33mCaso tenha algum arquivo ou pasta que você não queira adicionar ao backup, 
 coloque o caminho em \"config/ignore_list.conf\"\x1b[0m\n");
 
+    //TODO: Add the directories to vector dinamicaly
+    let vec_dir = vec!["Desktop", "Documentos", "Downloads", "Imagens", "Música", "Vídeos"];
     let mut pass_error = true;
     while pass_error {
         let password = rpassword::prompt_password("Digite a senha: ").unwrap();
@@ -15,13 +18,22 @@ coloque o caminho em \"config/ignore_list.conf\"\x1b[0m\n");
             //TODO: Run verify code here!
             ignore::start_ignore();
 
-            //TODO: Add threads for each backup::init
-            backup::init("Desktop", &password);
-            backup::init("Documentos", &password);
-            backup::init("Downloads", &password);
-            backup::init("Imagens", &password);
-            backup::init("Música", &password);
-            backup::init("Vídeos", &password);
+            let arc_password = Arc::new(password);
+            let mut count_handle: Vec<JoinHandle<_>> = Vec::new();
+            for i in 0..vec_dir.len() {
+                let current_directory = vec_dir[i];
+                let arc_password = Arc::clone(&arc_password);
+
+                let handle = thread::spawn(move || {
+                    backup::init(current_directory, arc_password.as_ref());
+                });
+
+                count_handle.push(handle);
+            }
+
+            for handle in count_handle {
+                handle.join().unwrap();
+            }
 
             ignore::end_ignore();
             pass_error = false;
